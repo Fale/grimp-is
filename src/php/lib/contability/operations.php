3807bba9762d4e41235db947038a65b33ee5108f
Fabio Alessandro Locati <fale@fale-laptop.(none)>
2009-09-20 19:01:17 +0200

    [php/lib/contability] Add the operations class (work still lacking)

---
+++ src/php/lib/contability/operations.php
@@ -0,0 +1,17 @@
+<?php
+
+class operations
+{
+    function add( $account, $import, $user, $to, $cause, $category )
+    {}
+
+    function view( $id )
+    {}
+
+//    function list( $account, $since, $to )
+//    {}
+}
+
+?>
+
+
