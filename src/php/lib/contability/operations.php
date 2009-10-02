138e1a3a45d718f633efa42d15c01171ec50c539
Fabio Alessandro Locati <fale@fale-laptop.(none)>
2009-10-02 13:31:30 +0200

    Implement the add operation (still lacking of dati() support)

--- src/php/lib/contability/operations.php
+++ src/php/lib/contability/operations.php
@@ -3,7 +3,13 @@
 class operations
 {
     function add( $account, $import, $user, $to, $cause, $category )
-    {}
+    {
+        $connession = mysql_connect( $db_host, $db_user ) or die( "Unable to open database" );
+        mysql_select_db( $db_db ) or die( "Unable to access database" );
+        $query = "INSERT INTO operations ( ID, dati, user, import, account, person, cause, category ) VALUES ( '', " . dati() . ", $user, $import, $account, $to, $cause, $category );";
+        $qresult = mysql_query( $query ) or die( mysql_error() );
+        return ( $qresult );
+    }
 
     function view( $id )
     {}
