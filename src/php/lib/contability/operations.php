0254279a431a78cff18c863f9580789e26504465
Fabio Alessandro Locati <fale@fale-laptop.(none)>
2009-10-02 13:36:06 +0200

    [php/lib/contability] Improve dati handleing

--- src/php/lib/contability/operations.php
+++ src/php/lib/contability/operations.php
@@ -1,12 +1,14 @@
 <?php
 
+require "../dati.h";
+
 class operations
 {
     function add( $account, $import, $user, $to, $cause, $category )
     {
         $connession = mysql_connect( $db_host, $db_user ) or die( "Unable to open database" );
         mysql_select_db( $db_db ) or die( "Unable to access database" );
-        $query = "INSERT INTO operations ( ID, dati, user, import, account, person, cause, category ) VALUES ( '', " . dati() . ", $user, $import, $account, $to, $cause, $category );";
+        $query = "INSERT INTO operations ( ID, dati, user, import, account, person, cause, category ) VALUES ( '', " . dati->get() . ", $user, $import, $account, $to, $cause, $category );";
         $qresult = mysql_query( $query ) or die( mysql_error() );
         return ( $qresult );
     }
